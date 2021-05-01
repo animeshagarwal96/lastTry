0532fc003cd82b6e98333d8f6243e9e9e969272a
Animesh <animeshagarwal19@gmail.com>
2021-05-01 18:54:24 +0530

    initial

--- just/models.py
+++ just/models.py
@@ -60,7 +60,6 @@ class Order(models.Model):
     product_name = models.CharField(max_length=100)
     product_price = models.CharField(max_length=100)
     product_category = models.CharField(max_length=100)
-    image = models.CharField(max_length=100,default="",blank=True)
     category_size = models.CharField(max_length=100,default="no size",blank=True)
     customer_name = models.CharField(max_length=100)
     customer_email = models.CharField(max_length=200)
--- just/views.py
+++ just/views.py
@@ -84,7 +84,6 @@ def checkout(request,id):
         product_name = product[0].product_name
         product_price = product[0].product_price
         product_category = product[0].category
-        image = product[0].image
         if product_category == 'Garments' or product_category=='Pants' or product_category =='Kurti':
             category_size = request.POST.get('size')
         else:
@@ -104,7 +103,7 @@ def checkout(request,id):
         if city.lower() != 'kolkata' or state.lower() != 'west bengal':
             messages.error(request,'We dont deliver this product to your place')
             return redirect(request.path)
-        order = Order(product_id=product_id,product_name=product_name,product_price=product_price,product_category=product_category,category_size=category_size,image=image,customer_name=customer_name,customer_email=customer_email,customer_phone=customer_phone,alternative_number=alternative_number,delivery_address=delivery_address,Alternate_address=Alternate_address,city=city,state=state,zipCode=zipCode,customer_username = customer_username)
+        order = Order(product_id=product_id,product_name=product_name,product_price=product_price,product_category=product_category,category_size=category_size,customer_name=customer_name,customer_email=customer_email,customer_phone=customer_phone,alternative_number=alternative_number,delivery_address=delivery_address,Alternate_address=Alternate_address,city=city,state=state,zipCode=zipCode,customer_username = customer_username)
         order.save()
         try:
             #Mail Owner
